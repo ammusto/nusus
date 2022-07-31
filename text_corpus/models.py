05b9e5f69252191b2c9b965d7675974601685220
antonio <amusto@gmail.com>
2022-07-31 01:21:57 -0400

    updated

--- text_corpus/models.py
+++ text_corpus/models.py
@@ -7,21 +7,20 @@ class Text(models.Model):
     text_id = models.IntegerField(primary_key=True)
     title_tl = models.CharField(max_length=250)
     title_ar = models.CharField(max_length=250)
+    au_id = models.ForeignKey('Author', on_delete = models.CASCADE)
+    gen_id = models.ForeignKey('Genre', on_delete = models.CASCADE)
+    descr = models.TextField()
+    perm = models.TextField()
+    permbib = models.TextField()
+    pdf = models.TextField()
     pg_len = models.IntegerField()
     pg_rng = models.CharField(max_length=25)
     word_len = models.IntegerField()
-    descr = models.TextField()
-    perm = models.TextField()
+    source = models.TextField()
     added = models.DateTimeField(default=timezone.now)
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    au_id = models.ForeignKey('Author', on_delete = models.CASCADE)
-    gen_id = models.ForeignKey('Genre', on_delete = models.CASCADE)
     published_date = models.DateTimeField(blank=True, null=True)
     avail_read = models.BooleanField()
-    descr_ar = models.TextField()
-    editions = models.TextField()
-    permbib = models.TextField()
-    source = models.TextField()
 
     def publish(self):
         self.save()
@@ -35,11 +34,11 @@ class Page(models.Model):
     pg_cont = models.TextField()
     pg_count = models.IntegerField()
     text = models.ForeignKey('Text', on_delete = models.CASCADE)
-    
+
 
     def __str__(self):
         return str(self.pg_id)
-        
+    
     def getContent(self):
         return str(self.pg_cont)
 
@@ -51,16 +50,13 @@ class Page(models.Model):
 class Author(models.Model):
     au_id = models.IntegerField(primary_key=True)
     au_tl = models.CharField(max_length = 250)
-    au_ar = models.CharField(max_length = 250)
     au_sh = models.CharField(max_length = 50)
+    au_ar = models.CharField(max_length = 250)
     sh_ar = models.CharField(max_length = 50)
     date = models.CharField(max_length = 50)
-    date_ar = models.CharField(max_length = 50)
-    cit = models.TextField()
     bio = models.TextField()
-    bio_ar = models.TextField()
-    books = models.TextField()
-    books_ar = models.TextField()
+    cit = models.TextField()
+
     def __str__(self):
         return self.au_tl
 
